079497b6125fdb127820e0754f16d3665bc227c2
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-26 12:15:05 -0500

    End of Class 09262018

---
+++ Student.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BuffTeks2
+{
+    public class Student
+    {
+        // STUDENT AND CLIENT HAVE WHAT IN COMMON (INHERITANCE -- BOTH HUMAN)
+
+        public int StudentID { get; set; }
+        
+
+        // attribute list
+        // PK
+        // firstName
+        // lastName
+        // Phone
+        // Email
+        // Role
+    }
+
+}
